7f7851880924903d4f16fb5f94803e2d250dea54
Yahor_Hlushak <Yahor_Hlushak@epam.com>
2023-10-08 11:25:29 +0300

    feat(module-4): complete module 4

--- js/custom.js
+++ js/custom.js
@@ -7,14 +7,69 @@
   });
 })();
 
+
+let isCommandPressed = false;
+window.addEventListener('keydown', event => {
+  if (event.key === 'Meta') {
+    isCommandPressed = true;
+  }
+})
+window.addEventListener('keyup', event => {
+  if (event.key === 'Meta') {
+    isCommandPressed = false;
+  }
+})
+
 document.querySelectorAll("#nav li").forEach(function(navEl) {
   navEl.onclick = function() {
     toggleTab(this.id, this.dataset.target);
   };
 
   navEl.onkeydown = function(event) {
-    if (event.key === 'Enter') {
-      toggleTab(this.id, this.dataset.target);
+    let isEventHandled = false;
+
+    switch (event.key) {
+      case 'Enter':
+      case ' ': {
+        toggleTab(this.id, this.dataset.target);
+        isEventHandled = true;
+        break;
+      }
+      case 'ArrowLeft': {
+        const targetTabId = this.id === '1' ? '3' : String(Number(this.id) - 1);
+
+        isCommandPressed
+            ? toggleTab('1', document.getElementById('1').dataset.target)
+            : toggleTab(targetTabId, document.getElementById(targetTabId).dataset.target);
+        isEventHandled = true;
+        break;
+      }
+      case 'ArrowRight': {
+        const targetTabId = this.id === '3' ? '1' : String(Number(this.id) + 1);
+
+        isCommandPressed
+            ? toggleTab('3', document.getElementById('3').dataset.target)
+            : toggleTab(targetTabId, document.getElementById(targetTabId).dataset.target);
+        isEventHandled = true;
+        break;
+      }
+      case 'Home': {
+        toggleTab('1', document.getElementById('1').dataset.target);
+        isEventHandled = true;
+        break;
+      }
+      case 'End': {
+        toggleTab('3', document.getElementById('3').dataset.target);
+        isEventHandled = true;
+        break;
+      }
+      default:
+        break;
+    }
+
+    if (isEventHandled) {
+      event.stopPropagation();
+      event.preventDefault();
     }
   };
 });
@@ -25,9 +80,12 @@ function toggleTab(selectedNav, targetId) {
   navEls.forEach(function(navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
+      navEl.setAttribute('aria-selected', 'true');
+      navEl.focus();
     } else {
       if (navEl.classList.contains("is-active")) {
         navEl.classList.remove("is-active");
+        navEl.setAttribute('aria-selected', 'false');
       }
     }
   });
@@ -56,3 +114,119 @@ document.querySelectorAll(".button.is-link.modal-button").forEach(function(linkE
   linkEl.addEventListener('click', (event) => focusCloseModalBtn(event.target.dataset.target, linkEl))
   linkEl.addEventListener('keydown', (event) => event.key === 'Enter' && focusCloseModalBtn(this.dataset.target, linkEl))
 });
+
+const dropdown = document.querySelector('.with-sublist');
+const dropdownContent  = document.getElementById('services-dropdown-options');
+
+const trapFocusInDropdown = () => {
+  const options = dropdownContent.querySelectorAll('li');
+  const firstOption = options[0];
+  const lastOption = options[options.length - 1];
+
+  firstOption.addEventListener('keydown', event => {
+    if (event.key === 'Tab' && event.shiftKey) {
+      lastOption.focus();
+      event.preventDefault();
+    }
+  })
+
+  lastOption.addEventListener('keydown', event => {
+    if (event.key === 'Tab') {
+      firstOption.focus();
+      event.preventDefault();
+    }
+  })
+}
+trapFocusInDropdown()
+
+const openDropdown = (focusLast) => {
+  dropdownContent.classList.remove('hidden');
+  dropdown.ariaExpanded = 'true';
+
+  if (focusLast) {
+    const options = dropdownContent.querySelectorAll('li');
+
+    options[options.length - 1].focus();
+  } else {
+    dropdownContent.querySelector('li').focus();
+  }
+};
+const closeDropdown = () => {
+  dropdownContent.classList.add('hidden');
+  dropdown.ariaExpanded = 'false';
+  dropdown.focus();
+}
+const toggleDropdown = (dropdownContentId) => {
+  if (dropdownContent.className.includes('hidden')) {
+    openDropdown()
+  } else {
+    closeDropdown()
+  }
+}
+dropdown.addEventListener('click', () => toggleDropdown())
+dropdown.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'Enter':
+    case ' ': {
+      toggleDropdown();
+      break;
+    }
+
+    default:
+      break;
+  }
+})
+
+navbarMenu.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeDropdown()
+  }
+})
+
+const menuList = document.querySelector('.navbar-end');
+
+menuList.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'ArrowLeft': {
+      const currentItemId = event.target.parentNode.id.split('-')[1];
+      const targetItemId = currentItemId === '1' ? '4' : String(Number(currentItemId) - 1);
+
+      isCommandPressed
+          ? document.querySelector(`#menu-1 button`).focus()
+          : document.querySelector(`#menu-${targetItemId} button`).focus();
+
+      break;
+    }
+    case 'ArrowRight': {
+      const currentItemId = event.target.parentNode.id.split('-')[1];
+      const targetItemId = currentItemId === '4' ? '1' : String(Number(currentItemId) + 1);
+
+      isCommandPressed
+          ? document.querySelector(`#menu-4 button`).focus()
+          : document.querySelector(`#menu-${targetItemId} button`).focus();
+      break;
+    }
+    case 'ArrowDown': {
+      if (event.target.className.includes('with-sublist')) {
+        openDropdown();
+      }
+      break;
+    }
+    case 'ArrowUp': {
+      if (event.target.className.includes('with-sublist')) {
+        openDropdown(true);
+      }
+      break;
+    }
+    case 'Home': {
+      document.querySelector(`#menu-1 button`).focus();
+      break;
+    }
+    case 'End': {
+      document.querySelector(`#menu-4 button`).focus();
+      break;
+    }
+    default:
+      break;
+  }
+})
